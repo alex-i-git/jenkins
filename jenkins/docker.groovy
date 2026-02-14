6aeb2a99cd70c7b59ade7e5c7aedee4ac5bd109f
alex-i-git <iostat.bsd@gmail.com>
2026-02-15 00:32:09 +0300

    fix param

--- jenkins/docker.groovy
+++ jenkins/docker.groovy
@@ -23,7 +23,7 @@ pipeline {
             steps {
                 echo "Building docker image"
                 dir ('helm-chart/docker/cli') {
-                    sh 'echo ${parameters.IMAGE_NAME}'
+                    sh "echo ${parameters.IMAGE_NAME}"
                 }
             }
         }
